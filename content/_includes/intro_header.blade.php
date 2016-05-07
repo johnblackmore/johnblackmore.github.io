2c63e39b1ed0b8aae2ac49386deaf19850747fc0
John Blackmore <john@blackmore.com>
2016-05-08 00:12:47 +0100

    font change and image mask on header images

--- content/_includes/intro_header.blade.php
+++ content/_includes/intro_header.blade.php
@@ -1,11 +1,13 @@
 <header class="intro-header" style="background-image: url('/img/headers/@yield('header::image')')">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="page-heading">
-                    <h1>@yield('header::title')</h1>
-                    <hr class="small">
-                    <span class="subheading">@yield('header::tagline')</span>
+    <div class="image-mask">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="page-heading">
+                        <h1>@yield('header::title')</h1>
+                        <hr class="small">
+                        <span class="subheading">@yield('header::tagline')</span>
+                    </div>
                 </div>
             </div>
         </div>
