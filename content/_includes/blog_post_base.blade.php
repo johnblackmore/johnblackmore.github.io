76068236d4c14ccba0a274f7df1cb26d7c1da3ad
John Blackmore <john@blackmore.com>
2016-05-10 17:30:13 +0100

    Added image mask to blog header

--- content/_includes/blog_post_base.blade.php
+++ content/_includes/blog_post_base.blade.php
@@ -5,12 +5,14 @@
 @section('body')
 {{--<!-- Page Header -->--}}
 <header class="intro-header" style="background-image: url('img/headers/@yield('header::image')')">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <div class="post-heading">
-                    <h1>@yield('post::title')</h1>
-                    <span class="meta">Posted on @yield('post::date') - <span class="eta"></span> read</span>
+    <div class="image-mask">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="post-heading">
+                        <h1>@yield('post::title')</h1>
+                        <span class="meta">Posted on @yield('post::date') - <span class="eta"></span> read</span>
+                    </div>
                 </div>
             </div>
         </div>
