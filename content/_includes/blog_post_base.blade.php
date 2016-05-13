a70f581b6bd800a36be864d2e9869de2774480ef
John Blackmore <john@blackmore.com>
2016-05-13 10:26:59 +0100

    Twitter and Facebook Metas

--- content/_includes/blog_post_base.blade.php
+++ content/_includes/blog_post_base.blade.php
@@ -2,57 +2,63 @@
 
 @section('header::image', 'candle-bokeh.jpg')
 
+@section('head')
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@@johnblackmore">
+    <meta name="twitter:creator" content="@@johnblackmore">
+    <meta name="twitter:title" content="@yield('post::title')">
+    <meta name="twitter:description" content="@yield('post::brief')">
+    <meta name="twitter:image" content="http://johnblackmore.com/img/headers/@yield('header::image')">
+    <meta property="og:url" content="{{ $currentUrlPath }}"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:title" content="@yield('post::title')"/>
+    <meta property="og:description" content="@yield('post::brief')"/>
+    <meta property="og:image" content="http://johnblackmore.com/img/headers/@yield('header::image')"/>
+@stop
+
 @section('body')
-{{--<!-- Page Header -->--}}
-<header class="intro-header" style="background-image: url('/img/headers/@yield('header::image')')">
-    <div class="image-mask">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <div class="post-heading">
-                        <h1>@yield('post::title')</h1>
-                        <h2 class="subheading">@yield('post::brief')</h2>
-                        <span class="meta">Posted on @yield('post::date') - <span class="eta"></span> read</span>
+    {{--<!-- Page Header -->--}}
+    <header class="intro-header" style="background-image: url('/img/headers/@yield('header::image')')">
+        <div class="image-mask">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <div class="post-heading">
+                            <h1>@yield('post::title')</h1>
+                            <h2 class="subheading">@yield('post::brief')</h2>
+                            <span class="meta">Posted on @yield('post::date') - <span class="eta"></span> read</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
 
-{{--<!-- Post Content -->--}}
-<article>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                @yield('post_body')
+    {{--<!-- Post Content -->--}}
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    @yield('post_body')
 
-                <div id="disqus_thread"></div>
-                <script>
-                    /**
-                     * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                     * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-                     */
-                    /*
-                     var disqus_config = function () {
-                     this.page.url = PAGE_URL; // Replace PAGE_URL with your page's canonical URL variable
-                     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                     };
-                     */
-                    (function() { // DON'T EDIT BELOW THIS LINE
-                        var d = document, s = d.createElement('script');
+                    <div id="disqus_thread"></div>
+                    <script>
+                        (function () {
+                            var d = document, s = d.createElement('script');
 
-                        s.src = '//johnblackmore.disqus.com/embed.js';
+                            s.src = '//johnblackmore.disqus.com/embed.js';
 
-                        s.setAttribute('data-timestamp', +new Date());
-                        (d.head || d.body).appendChild(s);
-                    })();
-                </script>
-                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+                            s.setAttribute('data-timestamp', +new Date());
+                            (d.head || d.body).appendChild(s);
+                        })();
+                    </script>
+                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript"
+                                                                      rel="nofollow">comments powered by Disqus.</a>
+                    </noscript>
+                </div>
             </div>
         </div>
-    </div>
-</article>
+    </article>
 @stop
 
 @section('scripts')
