f6a23f99fd15a47668caa3b555757873de8413ef
John Blackmore <john.blackmore@upad.co.uk>
2016-05-23 10:20:39 +0100

    google tag manager

--- content/_includes/base.blade.php
+++ content/_includes/base.blade.php
@@ -28,6 +28,15 @@
     @yield('head')
 </head>
 <body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MF4DCD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MF4DCD');</script>
+<!-- End Google Tag Manager -->
 {{--<!-- Navigation -->--}}
 <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
     <div class="container-fluid">
@@ -132,14 +141,6 @@
 <script src="@url('/js/clean-blog.min.js')"></script>
 {{-- Google Code Prettify --}}
 <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-306221-6', 'auto');
-    ga('send', 'pageview');
-</script>
 @yield('scripts')
 </body>
 </html>
