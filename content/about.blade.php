ab823be88dc19d5345453c385d2e463d5b5cffe2
John Blackmore <blackmorejd@googlemail.com>
2021-06-17 12:23:01 +0100

    about us page update

--- content/about.blade.php
+++ content/about.blade.php
@@ -1,6 +1,6 @@
 @extends('_includes.content_page')
 
-@section('pageTitle', 'About John Blackmore - Remote Freelance PHP Developer based in Taunton, UK')
+@section('pageTitle', 'About John Blackmore - Freelance Software & Digital Marketing Consultant based in Somerset, UK')
 @section('header::image', 'candle-bokeh.jpg')
 @section('header::title', 'Hire Me!')
 @section('header::tagline', 'Hello, is it me you&apos;re looking for?')
@@ -9,23 +9,23 @@
     @markdown
     ## About Me
 
-    Hello, I’m John. I am a **freelance PHP developer** and digital marketing specialist with over ten years of commercial experience in everything
-    from brochure websites to eCommerce, Software as a Service (SaaS) online applications and small enterprise management systems. **I work remotely** from my studio in Taunton, UK.
+    Hello, I’m John. I am a **freelance software and digital marketing consultant**, and former PHP developer with over ten years of commercial experience in everything
+    from brochure websites to eCommerce, Software as a Service (SaaS) online applications and small enterprise management systems. **I work remotely** from my studio in West Somerset, UK.
 
     ### Previous examples of work
 
     I have been lucky enough to work with some great companies, from startups to scale-ups and even some larger SMBs. During that time I have built some pretty interesting web applications including:
 
     - A **crowdfunding platform** that processed over &pound;100 million in direct debit and credit card transactions in one year, providing growth-funding for over 300 small businesses throughout the UK.
-    - An **online property rentals platform** that actively advertised and processed enquiries for up to 1500 properties a month. The same platform also provided end-to-end tenancy creation and management for thousands of landlords each year.
+    - An **online property rentals platform** that actively advertised and processed enquiries for up to 18,000 properties a year. The same platform also provided end-to-end tenancy creation and management for thousands of landlords each year.
     - An **insurance quote engine** used by a group of companies to quote and provide cover for thousands of tradespeople around the UK, offering several insurance products and combining over 30 risk factors to provide a unique quote for every customer.
     - A **Software-as-a-Service (SaaS) platform** that imported and aggregated pricing data for over 1 million used car sale prices each day, used to provide accurate vehicle pricing based on similar vehicles and price trends over time.
     - Consulted on and then implemented **improved developer workflows** for a UK growth-stage company, introducing local development builds, implementing continuous integration pipelines, and designed a **zero-downtime deployment platform** to ship code to staging and production environments.
 
     ### Technologies that I work with
 
-    As a developer I mainly work in PHP with a particular passion for building web applications on the Laravel framework, although I also
-    specialise in working on and modernising legacy PHP applications. Here are just some of the frameworks and technologies I use:
+    I primarily act in a consultant or project management capacity for my clients, working with a number of trusted developers to deliver a quality product, working to realistic deadlines at realistic prices. As a former developer, I mainly work with PHP projects with a particular passion for building web applications on the Laravel framework, although I can also
+    manage working on and modernising legacy PHP applications. Here are just some of the frameworks and technologies I have used:
 
     - **PHP Applications**: [Laravel Framework](https://laravel.com/), [Symfony Framework](https://symfony.com/), [CodeIgniter](https://codeigniter.com/)
     - **Frontend**: [Livewire](https://laravel-livewire.com/), [Vue.js](https://vuejs.org/), [jQuery](https://jquery.com/)
@@ -37,16 +37,16 @@
     I am always looking for interesting projects to work on. I have worked with startups, scale-ups, and larger companies for both hands-on development and consulting roles. Here are some of the types of projects that I have a particularly good knowledge of:
 
     - **Re-architecting legacy PHP applications**: If you have an older PHP application (over 3 years) or a system that was previously built off-shore or by contractors, you may be considering your options for maintaining that system. I can help evaluate your current application and give guidance on how best to either maintain or re-write the system using modern PHP methodologies and best practices.
-    - **Maintaining older applications**: Laravel and Symfony are the big names in town, but there are still loads of working CodeIgniter and Yii applications in the wild. It isn't easy finding a more experienced developer who understands how these frameworks were designed, I can help you.
+    - **Maintaining older applications**: Laravel and Symfony are the big names in town, but there are still loads of working CodeIgniter and Yii applications in the wild. It isn't easy finding developers who understand how these frameworks were designed, I can help you.
     - **Scaling existing applications**: As traffic increases to your web application, you will eventually need more servers. With proper planning and re-working the application to support multi-server setups, I have adapted applications to run automatically and seamlessly scale to over 150 servers in a cloud-based configuration.
     - **Improved Developer Workflows**: You might already have a team in place, but you want to get a better handle on local development and deploying your code? I have built many development and deployment processes that allow you to reliably and confidently ship code without having to work late nights and weekends. I have worked in teams where we were able to ship to production with zero-downtime, enabling us to ship upwards of 30 code changes a day during working hours.
 
     With all projects, I take a **pragmatic and evidence-based approach**. I will work with the business to ensure the correct project is built at the correct time, to ensure the desired outcome. If I don't know what that correct path looks like, I will be up-front and honest, and do whatever I can to find someone who can help if required.    
 
 
-    ### Available to Hire
+    ### Availability to Hire
 
-    I am currently available to hire for one-off projects and on-going contract work (1-5 days a week). I am a **remote worker based in Taunton**, Somerset working from my studio, although I am also able to attend on-site meetings as required (expenses paid). I would be more than happy to discuss any requirements you might have, so please give me a call or send an email to see what I can do for you.
+    Due to current demand, I am unable to take on new clients until around October 2021. However, I would be more than happy to discuss any requirements you might have, so please give me a call or send an email to see what I can do for you. Please note, I am a **remote worker based in Taunton**, Somerset working from my studio. I am not currently travelling to meet clients, although I'm happy to jump on a video call to discuss your proposal.
 
     - Telephone: 01823 240246
     - Email: [hello@johnblackmore.com](mailto:hello@johnblackmore.com)
